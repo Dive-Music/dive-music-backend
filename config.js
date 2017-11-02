9ba42dabfce89a6990423f26b628d019aa3847be
Curtis Young <curtiscyoung@gmail.com>
2017-11-02 14:54:38 -0600

    Change to config.js

--- config.js
+++ config.js
@@ -1,7 +1,4 @@
 module.exports = {
-    port : process.env.PORT || 8800,
-    db : process.env.DB,
-    secret : process.env.SECRET,
-    data: process.env.DATA,
+    port : process.env.PORT || 7800,
     database: "mongodb://ccy:raprap@ds243335.mlab.com:43335/dive-reviews"
 }
